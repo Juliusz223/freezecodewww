d367fc1f988c2f3036d165970c123b08a1b0527d
Juliusz223 <juliuszkrol2013@gmail.com>
2025-06-15 19:18:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-const token = "MTM3MDEzOTM3MzYzODUyMDg1Mw.GpJOUe.Vr0qcKb7Ashrd_n0vZn9sNYhwiZdKHNv_dDLD8"; // NIE udostępniaj tego publicznie!
-const guildId = "1368550324348129391";
+const guildId = "1368550324348129391"; // Twój Discord Server ID
 
-fetch(`https://discord.com/api/guilds/${guildId}?with_counts=true`, {
-    headers: {
-        "Authorization": `Bot ${token}`
-    }
-})
-.then(response => response.json())
-.then(data => {
-    document.getElementById("memberCount").innerText = data.approximate_member_count;
-})
-.catch(err => {
-    console.error(err);
+fetch(`https://discord.com/api/guilds/${guildId}/widget.json`)
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById("memberCount").innerText = data.presence_count;
+  })
+  .catch(() => {
     document.getElementById("memberCount").innerText = "Błąd";
-});
+  });
